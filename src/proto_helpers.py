a60f963b2d766d11e794e073fd636e39aae73c1e
Alexander Holyoke <aholyoke@edu.uwaterloo.ca>
2017-10-31 18:03:54 -0400

    Fix corner cases for naming converntions

--- src/proto_helpers.py
+++ src/proto_helpers.py
@@ -2,16 +2,16 @@
 def proto_counter(name):
 	if name[0] == '$':
 		name = name[1:]
-	return "counter_{}".format(name)
+	return "counter_{}".format(name).lower()
 
 
 def proto_scalar(name):
 	if name[0] == '%':
 		name = name[1:]
-	return "scalar_{}".format(name)
+	return "scalar_{}".format(name).lower()
 
 
 def proto_set(name):
 	if name[0] == '#':
 		name = name[1:]
-	return "set_{}".format(name)
+	return "set_{}".format(name).lower()
